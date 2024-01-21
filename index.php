1a1d640ed689adadcbc3457a522ad160313897e7
HalilBatuhan <67440436+HalilBatuhan@users.noreply.github.com>
2024-01-22 00:48:09 +0300

    22.01.24 00:48

--- index.php
+++ index.php
@@ -46,6 +46,7 @@ if (isset($_POST["giris"])) {
 <html lang="en">
 
 <head>
+    <title>Parsel Sorgu</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
